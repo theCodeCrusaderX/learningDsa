c686bfae4f132d36f1c026d20a714ca115e00b19
Dinesh726544 <bobi726544@gmail.com>
2024-01-20 09:40:10 +0530

    complete opps basic maths recursion

--- cppstl/code.cpp
+++ cppstl/code.cpp
@@ -48,7 +48,7 @@ void explainvector()
         cout << *(it) << " ";
     }
 
-    for(auto it = v.begin(); it != v.end(); it++) // auto matically adjust the datatype 
+    for(auto it = v.begin(); it != v.end(); it++) // auto matically adjust the datatype since v.begin() function is an iterator datatype so datatype of it is type of iterator
     {
         cout << *(it) << " ";
     }
@@ -84,9 +84,223 @@ void explainvector()
 
 }
 
+void explainList()
+{
+    list<int> ls;
+
+    ls.push_back(2); // {2}
+    ls.emplace_back(5); // {2,5}
+    // in vector we use inset function to inset element anywhere but that takes more and more time complexity.
+
+    // in list we are having push_front
+
+    ls.push_front(88); //{88,2,5}
+    ls.emplace_front(44); //{44,88,2,5}
+
+    //rest function same as vector 
+    //begin end clear insert size swap rbegin rend
+}
+
+void explainDeque()
+{
+    deque<int> dq;
+    dq.push_back(2); // {2}
+    dq.emplace_back(5); // {2,5}
+    dq.push_front(88); //{88,2,5}
+    dq.emplace_front(44); //{44,88,2,5}
+
+    dq.pop_back(); //{44,88,2}
+    dq.pop_front(); //{88,2}
+
+    dq.back(); // gives last element i.e 2
+    dq.front(); //gives front element i.e 88
+
+    //rest function same as vector 
+    //begin end clear insert size swap rbegin rend
+    
+}
+
+void explainStack()
+{
+    stack<int> st;
+
+    //last in first out
+
+    //main function push pop top having time complexity O(1).
+    st.size(); //gives size of stack 
+    st.empty(); //gives 1 for no elements in stack
+    
+}
+
+void explainQueue()
+{
+    queue<int> q;
+
+    //first in first out
+
+    q.push(1); //{1}
+    q.push(2); //{1,2}
+    q.emplace(3); //{1,2,3}
+
+    //back front pop  size swap empty same as stack.
+
+    //all the operations are having O(1) time complexity.
+
+
+}
+
+void explain_priority_queue()
+{
+    priority_queue<int> pq;  //also called max heap
+
+    pq.push(3); //{3}
+    pq.emplace(2); //{3,2}
+    pq.push(10); //{10,3,2}
+    pq.push(1); // {10,3,2,1}
+
+    cout << pq.top(); //10
+    pq.pop(); //{3,2,1}
+
+    //size swap empty function same as others
+
+    priority_queue<int, vector<int>, greater<int>> pq; //min heap
+
+    pq.push(11); //{11}
+    pq.emplace(2); //{2,11}
+    pq.push(10); //{2,10,11}
+
+    //time complexity:-
+    //push and pop = log n
+    //top = O(1)
+}
+
+void explainSet() {
+
+    //stores everything in sorted order and unique in nature
+
+    set<int> st;
+    st.insert(1); // {1}
+    st.emplace(2); //{1,2}
+    st.insert(2); //{1,2}
+    st.insert(13); //{1,2,13}
+    st.insert(8); //{1,2,8,13}
+    //functionality of insert in vector can be used also that only increases efficiency
+
+    //begin() end() rbegin() rend() size() empty() and swap() are same as those of above
+
+    // {1,2,3,4,5}
+    auto it = st.find(3); //as find is an iterator function 
+
+    // {1,2,3,4,5}
+    auto it = st.find(6); //now it will point to st.end() which is right after 5
+
+    //{1,4,5}
+    st.erase(5); //erase 5 //takes logarithmic time
+
+    int cnt = st.count(1);
+
+    auto it = st.find(3);
+    st.erase(it); //it takes constant time
+
+    // {1,2,3,4,5}
+    auto it1 = st.find(2);
+    auto it2 = st.find(4);
+    st.erase(it1,it2); //after erase {1,4,5} [first,last)
+
+
+    //lower and upper bound
+    //lower bound :- points to thats element if present if not then next to that element which is greater to that element
+
+    auto it = st.lower_bound(2);
+
+    // upper bound :- points to next to that which is greater to that element if present, if not then next to that element which is greater to that element
+
+    auto it = st.upper_bound(4);
+
+
+    //time complexity for all operation are log n
+}
+void explainMultiSet() {
+    //everything is same as set only differ is that it can store diffrent value too..
+    multiset<int> ms;
+    ms.insert(1); //{1}
+    ms.insert(2); //{1,2}
+    ms.insert(1); //{1,1,2}
+
+    ms.erase(1); //all 1's erased
+    int cnt = ms.count(1);
+
+    //only a single one erased
+    ms.erase(ms.find(1));
+}
+void explainUnorderedSet() {
+
+    //randamised order in unique way also
+
+    unordered_set<int> st;
+
+    //only lower and upper bound function not work rest are same as set
+
+    //time complexity is O(1) most of the cases but in wrost case O(n)
+}
+
+void explainMap() {
+
+    //unique key in sorted order.
+
+    map<int,int> mpp;
+    // map<int,pair<int,int>> mpp;
+    // map<pair<int,int>, int> mpp;
+
+    mpp[1] = 2;
+    mpp.emplace({3,1});
+    mpp.insert({2,4});
+    mpp[{2,3}] = 10;
+
+    for(auto it : mpp) {
+        cout << it.first << " " << it.second << endl;
+    }
+
+    cout << mpp[1];
+    cout << mpp[5];
+
+
+    auto it = mpp.find(3);
+    cout << *(it).second;
+
+    auto it mpp.find(5);
+
+    // This is the syntax
+    auto it mpp.lower_bound(2);
+    auto it = mpp.upper_bound(3);
+
+    // erase, swap, size, empty, are same as above
+
+    // time complexity:- log n
+}
+
+void explainMultimap() {
+    //everything same as map, only it can store multiple keys
+    //only mpp[key] cant be used here
+}
+
+void explainUnorderedMap() {
+    //O(1) and in wrost case O(n).
+}
 int main() 
 {
     expainpair();
     explainvector();
+    explainList();
+    explainDeque();
+    explainStack();
+    explainQueue();
+    explain_priority_queue();
+    explainSet();
+    explainMultiSet();
+    explainUnorderedSet();
+    explainMap();
+    explainMultimap();
+    explainUnorderedMap();
     return 0;
 }
---
+++ opps/encapsulation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class ABC {
+    int x;
+
+    public:
+
+    void set(int n) {
+        x=n;
+    }
+
+    int get() {
+        return x;
+    }
+};
+
+int main() {
+
+    ABC obj1;
+    obj1.set(3);
+    cout << obj1.get();
+
+    return 0;
+}
